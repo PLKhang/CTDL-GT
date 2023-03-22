555980256ce0def685af56d9ebaf928ecf4d339d
Lê Phúc Khang <plkhang090403@gmail.com>
2023-03-22 12:53:16 +0700

    update DiemThi

--- ThiTracNghiem/struct.cpp
+++ ThiTracNghiem/struct.cpp
@@ -1,184 +1,139 @@
 #include "struct.h"
 #include <iostream>
-#include<cstdlib>
-#include<ctime>
-#include<math.h>
-#include<string>
-#include<vector>
+#include <cstdlib>
+#include <ctime>
+#include <math.h>
+#include <string>
+#include <vector>
 
 //---------------------------MonHoc--------------------------//
 
 //---------------------------CauHoi--------------------------//
 
 //---------------------------DiemThi--------------------------//
-bool is_Empty_DT(PtrDT &first)
+bool is_Empty_DT(PtrDT first)
 {
     if (first == NULL)
         return 1;
     return 0;
 }
-bool is_Full_DT(PtrDT &first, ListMH dsmh)
+bool is_Full_DT(PtrDT first)
 {
     if (is_Empty_DT(first))
         return 0;
     int count = 0;
-    PtrDT p = new nodeDiemThi;
-    for (p = first; p->next != NULL; p = p->next)
-        count++;
-    if (count < dsmh.n) // dsmh == tên khai báo biến của danh sách môn học(khai báo trong main.cpp)
-        return 0;
+    PtrDT p;
+    for (p = first; p != NULL; p = p->next)
+        if (p->info.diemThi < 0)
+            return 0;
     return 1;
 }
-bool is_Existed_MaMon(PtrDT &first, char *maMon)
+bool is_Existed_DT(PtrDT first, char *maMon)
 {
     if (is_Empty_DT(first))
         return 0;
-    PtrDT p = new nodeDiemThi;
-    for (p = first; p->next != NULL; p = p->next)
+    PtrDT p;
+    for (p = first; p != NULL; p = p->next)
+        if (p->info.maMonHoc == maMon)
+            if (p->info.diemThi >= 0)
+                return 1;
+    return 0;
+}
+
+bool is_Existed_MaMH_DT(PtrDT first, char *maMon)
+{
+    if (is_Empty_DT(first))
+        return 0;
+    PtrDT p;
+    for (p = first; p != NULL; p = p->next)
         if (p->info.maMonHoc == maMon)
             return 1;
     return 0;
 }
-
-void insert_First_DT(PtrDT &first, DiemThi x)
+bool insert_First_DT(PtrDT &first, DiemThi x)
 {
-    if (is_Full_DT(first))
-        return;
     PtrDT p = new nodeDiemThi;
     p->info = x;
     p->next = first;
     first = p;
+    return 1;
 }
-void insert_Last_DT(PtrDT &first, DiemThi x)
+
+bool insert_After_DT(PtrDT p, DiemThi x)
 {
-    if (is_Full_DT(first))
-        return;
-    PtrDT p = new nodeDiemThi;
-    p->info = x;
-    p->next = NULL;
-    PtrDT q = first;
-    while (q->next != NULL)
-        q = q->next;
-    q->next = p;
-}
-void insert_After_DT(PtrDT p, DiemThi x)
-{
-    // if(is_Full_DT(first)) // how to check it? :v
-    //     return;
-    if (p == NULL || p->next == NULL)
-        return;
+    if (p == NULL)
+        return 0;
     PtrDT q = new nodeDiemThi;
     q->info = x;
     q->next = p->next;
     p->next = q;
+    return 1;
 }
 
-void delete_First_DT(PtrDT &first)
+bool insert_Order_DT(PtrDT &first, DiemThi x)
+{
+    PtrDT u, v;
+    for (u = first; u->info.maMonHoc > x.maMonHoc; v = u, u = u->next)
+        ;
+    if (u == first)
+        insert_First_DT(first, x);
+    else
+        insert_After_DT(v, x);
+    return 1;
+}
+
+bool delete_First_DT(PtrDT &first)
 {
     if (is_Empty_DT(first))
-        return;
-    PtrDT p = new nodeDiemThi;
-    p = first;
+        return 0;
+    PtrDT p = first;
     first = p->next;
     delete p;
+    return 1;
 }
-void delete_Last_DT(PtrDT &first);
-void delete_After_DT(PtrDT &first, PtrDT p)
+
+bool delete_After_DT(PtrDT p)
 {
-    if (is_Empty_DT(first))
-        return;
     if (p == NULL || p->next == NULL)
-        return;
+        return 0;
     PtrDT q = p->next;
     p->next = q->next;
     delete q;
+    return 1;
 }
-void delete_List_DT(PtrDT &first)
+bool delete_Pos_DT(PtrDT &first, char *maMon)
+{
+    if (is_Empty_DT(first) || !is_Existed_MaMH_DT(first, maMon))
+        return 0;
+    PtrDT pos = pos_MaMH_DT(first, maMon);
+    pos->info.diemThi = -1;
+    return 1;
+}
+
+bool delete_List_DT(PtrDT &first)
 {
     while (first != NULL)
-        delete_First_DT(first);
+        if (!delete_First_DT(first))
+            return 0;
+    return 1;
 }
-void delete_Info_DT(PtrDT &first, char *maMon);
 
-bool search_info_DT(PtrDT first, char *maMon)
+bool set_DiemThi_DT(PtrDT &p, float x)
 {
-    if (is_Empty_DT(first))
+    if (p == NULL)
         return 0;
-    PtrDT p = new nodeDiemThi;
-    for (p = first; p->next != NULL; p = p->next)
-        if (p->info.maMonHoc == maMon)
-            return 1;
-    return 0;
+    p->info.diemThi = x;
+    return 1;
 }
-void set_Info_DT(PtrDT &p, DiemThi x)
-{
-    p->info = x;
-}
-PtrDT pos_MonHoc_DT(PtrDT first, char *maMon)
+PtrDT pos_MaMH_DT(PtrDT first, char *maMon)
 {
     if (is_Empty_DT(first))
         return NULL;
-    PtrDT p = first;
-    if (p->info.maMonHoc == maMon)
-        return p;
+    PtrDT p = NULL;
 
-    while (p->next->info.maMonHoc != maMon && p->next != NULL)
-    {
-        p = p->next;
-    }
-    return p->next;
-}
-int counting_Score_DT(PtrDT first, float x, int types)
-{
-    if (is_Empty_DT(first))
-        return 0;
-
-    int count = 0;
-    PtrDT p = new nodeDiemThi;
-
-    if (types > 0)
-        for (p = first; p->next != NULL; p = p->next)
-            if (p->info.diemThi > x)
-                count++;
-    if (types < 0)
-        for (p = first; p->next != NULL; p = p->next)
-            if (p->info.diemThi < x)
-                count++;
-    if (types == 0)
-        for (p = first; p->next != NULL; p = p->next)
-            if (p->info.diemThi == x)
-                count++;
-    return count;
-}
-
-int compare_Float_Number(float num1, float num2, int precision)
-{
-    float threshold = pow(0.1, precision); // calculate threshold based on precision
-    float diff = num1 - num2;
-    if (abs(diff) < threshold)
-    {
-        return 0; // num1 = num2
-    }
-    else if (diff > 0)
-    {
-        return 1; // num1 > num2
-    }
-    else
-    {
-        return -1; // num1 < num2
-    }
-}
-
-void print_List_DT(PtrDT first)
-{
-    if (is_Empty_DT(first))
-        return;
-    PtrDT p = new nodeDiemThi;
-    for (p = first; p->next != NULL; p = p->next)
-    {
-        std::cout << p->info.maMonHoc << '\t';
-        std::cout << p->info.diemThi << '\n';
-    }
+    for (p = first; p->info.maMonHoc != maMon && p != NULL; p = p->next)
+        ;
+    return p;
 }
 
 //---------------------------SinhVien--------------------------//
--- ThiTracNghiem/struct.h
+++ ThiTracNghiem/struct.h
@@ -26,15 +26,14 @@ struct CauHoi
     int ID;
     char maMonHoc[16];
     unsigned short answer;
-    char ans1[100], ans2[100], ans3[100], ans4[100], question[200]; 
-
+    char ans1[100], ans2[100], ans3[100], ans4[100], question[200];
 };
-typedef CauHoi*ptrCauHoi;
+typedef CauHoi *ptrCauHoi;
 struct nodeCauHoi
 {
-    CauHoi info; //key = info.ID;
-    nodeCauHoi *left=NULL;
-    nodeCauHoi *right=NULL;
+    CauHoi info; // key = info.ID;
+    nodeCauHoi *left = NULL;
+    nodeCauHoi *right = NULL;
 };
 
 typedef nodeCauHoi *STreeCH;
@@ -66,10 +65,10 @@ struct SinhVien
 struct nodeSinhVien
 {
     SinhVien info;
-    nodeSinhVien* next;
+    nodeSinhVien *next;
 };
 
-typedef nodeSinhVien* PtrSV;
+typedef nodeSinhVien *PtrSV;
 //------------------------------------------------//
 struct LopHoc
 {
@@ -82,13 +81,11 @@ struct LopHoc
 struct DanhSachLopHoc // MANG CON TRO
 {
     int soLuong = 0;
-    LopHoc* nodes[MaxOfClasses];
+    LopHoc *nodes[MaxOfClasses];
 };
 
 typedef DanhSachLopHoc ListLH;
 //------------------------------------------------//
-
-
 
 //-------------------MonHoc------------------//
 /*
@@ -98,55 +95,53 @@ typedef DanhSachLopHoc ListLH;
 */
 
 //-------------------CauHoi------------------//
-int CreateID(int Number[],int &i);
-void PreTraversal(CauHoi save[],STreeCH root,int &i);
-void InsertQuestion(STreeCH &root,STreeCH question);
+int CreateID(int Number[], int &i);
+void PreTraversal(CauHoi save[], STreeCH root, int &i);
+void InsertQuestion(STreeCH &root, STreeCH question);
 STreeCH balance(STreeCH root);
-CauHoi* GetQuestion(STreeCH &root,int number_question);
-//void Read(STreeCH &root);
+CauHoi *GetQuestion(STreeCH &root, int number_question);
+// void Read(STreeCH &root);
 
 //-------------------DiemThi------------------//
 
-void KhoiTao_PtrDT(PtrDT &First)
+void KhoiTao_PtrDT(PtrDT First)
 {
     First = NULL;
 }
 
-bool is_Empty_DT(PtrDT &first);
-bool is_Full_DT(PtrDT &first);
-bool is_Existed_MaMon(PtrDT &first, char * maMon);
+// kiem tra dsdt co node nao hay chua
+bool is_Empty_DT(PtrDT first);
+// kiem tra neu tat ca cac mon hoc deu co diem(sai neu ton tai diem < 0)
+bool is_Full_DT(PtrDT first);
+// kiem tra neu MonHoc da co diem (diem >= 0)
+bool is_Existed_DT(PtrDT first, char *maMon);
+// kiem tra MonHoc co trong ds hay chua(truong hop: THEM MON HOC)
+bool is_Existed_MaMH_DT(PtrDT first, char *maMon);
+bool insert_First_DT(PtrDT &first, DiemThi x);
+bool insert_After_DT(PtrDT p, DiemThi x);
+// them vao sap xep theo ma mon (truong hop: THEM MON HOC)
+bool insert_Order_DT(PtrDT &first, DiemThi x);
 
-void insert_First_DT(PtrDT &first, DiemThi x);
-void insert_Last_DT(PtrDT &first, DiemThi x);
-void insert_Order_DT(PtrDT &first, DiemThi x); // them vao sap xep theo ma mon
-void insert_After_DT(PtrDT &p, DiemThi x);
+bool delete_DiemThi_First_DT(PtrDT &first);
+bool delete_DiemThi_After_DT(PtrDT p);
+bool delete_Pos_DT(PtrDT &first, char *maMon);
+// Xoa danh sach diem thi cua sinh vien
+bool delete_List_DT(PtrDT &first);
+// void delete_Info_DT(PtrDT &first, DiemThi x);
 
-void delete_First_DT(PtrDT &first);
-void delete_Last_DT(PtrDT &first);
-void delete_After_DT(PtrDT &first, PtrDT p);
-void delete_List_DT(PtrDT &first);
-void delete_Info_DT(PtrDT &first, DiemThi x);
-
-bool search_Info_DT(PtrDT &first, char * maMon);
-void set_Info_DT(PtrDT &first, DiemThi x);
-PtrDT pos_MonHoc_DT(PtrDT &first, char *maMon);
-int counting_Score_DT(PtrDT &first, float x, int types);
-int compare_Float_Number(float num1, float num2, int precision = 2);
-
-void print_List_DT(PtrDT first);
+// Dung ham pos_MaMH_DT() truyen vao de lay vi tri sua diemThi
+bool set_DiemThi_DT(PtrDT &p, float x);
+// Tra ve vi tri node chua MaMH
+PtrDT pos_MaMH_DT(PtrDT first, char *maMon);
 
 //-------------------SinhVien------------------//
 
-void KhoiTao_PtrSV(PtrSV& First)
+void KhoiTao_PtrSV(PtrSV &First)
 {
     First = NULL;
 }
 
-/*
-
-(some functions here)
-
-*/
+bool is_Empty_SV(PtrSV first);
 
 //-------------------LopHoc------------------//
 
@@ -154,19 +149,19 @@ bool is_Empty_LH(ListLH &ListLH);
 bool is_Full_LH(ListLH &ListLH);
 bool is_Existed_MaLop(ListLH &ListLH, char maLop[]);
 
-//void FunctionLop(ListLH &ListLH);// HAM DO HOA
+// void FunctionLop(ListLH &ListLH);// HAM DO HOA
 
 void ThemLop(ListLH &ListLH, LopHoc);
 
-//void NhapLop(ListLH &ListLH, LopHoc);
+// void NhapLop(ListLH &ListLH, LopHoc);
 
-void XoaLop (ListLH &ListLH, int &page);
-//void NhapLopXoa(ListLH &ListLH,int &page);
+void XoaLop(ListLH &ListLH, int &page);
+// void NhapLopXoa(ListLH &ListLH,int &page);
 
 void SuaLop(ListLH &ListLH, int state, int page);
 void ChinhLop(ListLH &ListLH, int &page);
-//void NhapLopChinh(ListLH &ListLH , int &page);
+// void NhapLopChinh(ListLH &ListLH , int &page);
 
 void InDSLH(ListLH ListLH);
 
-#endif //STRUCT_H
+#endif // STRUCT_H
