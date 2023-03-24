fd370888c0b8be5bc3326f5dda477b8da17a4c1b
Lê Phúc Khang <plkhang090403@gmail.com>
2023-03-24 12:39:16 +0700

    ghi dssv, dsdt

--- ThiTracNghiem/docGhiFile.cpp
+++ ThiTracNghiem/docGhiFile.cpp
@@ -1,7 +1,7 @@
 #include "docGhiFile.h"
 int doc_danhSachLopHoc(ListLH &dslh)
-{   
-    //Mo file
+{
+    // Mo file
     ifstream file;
     file.open("Data/DanhSachLopHoc.txt", ios::in);
     if (!file)
@@ -17,14 +17,14 @@ int doc_danhSachLopHoc(ListLH &dslh)
     string field;
 
     // vong lap de doc cac dong trong file
-    while(getline(file, line)) 
+    while (getline(file, line))
     {
         pos = 0;
         field_Num = 1;
         while ((pos = line.find("|")) != string::npos) // dk: toi khi pos tim thay dau | va chuoi khac gia tri cuoi
         {
             field = line.substr(0, pos); // cat chuoi dai thanh chuoi con tu dau den vi tri pos -> luu vao field
-            line.erase(0, pos + 1); // xoa chuoi trong line tu o den pos + 1
+            line.erase(0, pos + 1);      // xoa chuoi trong line tu o den pos + 1
             switch (field_Num)
             {
             case 1:
@@ -41,11 +41,11 @@ int doc_danhSachLopHoc(ListLH &dslh)
             }
             field_Num++;
         }
+        doc_danhSachSinhVien(data.danhSachSinhVien, data.maLop);
     }
-    doc_danhSachSinhVien(data.danhSachSinhVien, data.maLop);
 
     // Dong file
-    file.close(); 
+    file.close();
 }
 int doc_danhSachSinhVien(PtrSV &dssv, string maLH)
 {
@@ -144,7 +144,7 @@ int doc_danhSachDiemThi(PtrDT &dsdt, string maLH, string mssv)
 }
 int doc_danhSachMonHoc(ListMH &dsmh)
 {
-    //Mo file
+    // Mo file
     ifstream file;
     file.open("Data/DanhSachMonHoc.txt", ios::in);
     if (!file)
@@ -160,14 +160,14 @@ int doc_danhSachMonHoc(ListMH &dsmh)
     string field;
 
     // vong lap de doc cac dong trong file
-    while(getline(file, line)) 
+    while (getline(file, line))
     {
         pos = 0;
         field_Num = 1;
         while ((pos = line.find("|")) != string::npos) // dk: toi khi pos tim thay dau | va chuoi khac gia tri cuoi
         {
             field = line.substr(0, pos); // cat chuoi dai thanh chuoi con tu dau den vi tri pos -> luu vao field
-            line.erase(0, pos + 1); // xoa chuoi trong line tu o den pos + 1
+            line.erase(0, pos + 1);      // xoa chuoi trong line tu o den pos + 1
             switch (field_Num)
             {
             case 1:
@@ -183,97 +183,139 @@ int doc_danhSachMonHoc(ListMH &dsmh)
         }
     }
     // Dong file
-    file.close(); 
+    file.close();
 }
-int doc_danhSachCauHoi(STreeCH &dsch){
+int doc_danhSachCauHoi(STreeCH &dsch)
+{
     ifstream docfile("Data/DanhSachCauHoi.txt");
     STreeCH question;
     string temp;
-    if(!docfile)return 0;
-    while(!docfile.eof()){
-        question=new nodeCauHoi;
-        docfile.getline(question->info.maMonHoc,16);
-        docfile>>question->info.ID;
+    if (!docfile)
+        return 0;
+    while (!docfile.eof())
+    {
+        question = new nodeCauHoi;
+        docfile.getline(question->info.maMonHoc, 16);
+        docfile >> question->info.ID;
         docfile.ignore();
-        docfile.getline(question->info.question,200);
-        docfile.getline(question->info.ans1,100);
-        docfile.getline(question->info.ans2,100);
-        docfile.getline(question->info.ans3,100);
-        docfile.getline(question->info.ans4,100);
-        docfile>>question->info.answer;
+        docfile.getline(question->info.question, 200);
+        docfile.getline(question->info.ans1, 100);
+        docfile.getline(question->info.ans2, 100);
+        docfile.getline(question->info.ans3, 100);
+        docfile.getline(question->info.ans4, 100);
+        docfile >> question->info.answer;
         docfile.ignore();
-        InsertQuestion(dsch,question);
+        InsertQuestion(dsch, question);
     }
     docfile.close();
 }
 int ghi_danhSachLopHoc(ListLH dslh)
 {
-    //Mo file
+    // Mo file
     ofstream file;
     file.open("Data/DanhSachLopHoc.txt", ios::out);
     if (!file)
         return 0;
 
     file << dslh.n << endl;
-    for(int i = 0; i < dslh.n; i++)
+    for (int i = 0; i < dslh.n; i++)
     {
         // cap du lieu
         LopHoc data;
-        file << data.maLop << "|" ;
-		file << data.tenLop << "|" ;
+        file << data.maLop << "|";
+        file << data.tenLop << "|";
         file << data.nienKhoa << "|";
         // Ghi dữ liệu vào file
         // cin >> data.maLop;
         // cin.ignore();
         // getline(cin, data.tenLop);
         // cin >> data.nienKhoa;
-        //data.danhSachSinhVien = ghi_danhSachSinhVien();
-        //file << data.maLop << "|" << data.tenLop << "|" << data.nienKhoa << "|"  << endl;
-        //ghi_danhSachSinhVien(data.danhSachSinhVien, data.maLop);
-        file << endl;
+        // data.danhSachSinhVien = ghi_danhSachSinhVien();
+        // file << data.maLop << "|" << data.tenLop << "|" << data.nienKhoa << "|"  << endl;
+        // ghi_danhSachSinhVien(data.danhSachSinhVien, data.maLop);
+        file << '\n';
     }
 
     // Đóng file
     file.close();
     return 1;
 }
+int ghi_danhSachSinhVien(PtrSV dssv, string maLH)
+{
+    string fileLocation = "Data/DanhSachSinhvien/" + maLH + ".txt";
+    ofstream file(fileLocation);
+    if (!file)
+        return 0;
+    PtrSV p = dssv;
+    while (p != NULL)
+    {
+        file << p->info.MSSV << '|' << p->info.password << '|' << p->info.ho << '|' << p->info.ten << '|' << p->info.phai << "|\n";
+        ghi_danhSachDiemThi(p->info.danhSachDiemThi, maLH, p->info.MSSV);
+        p = p->next;
+    }
+    return 1;
+}
+int ghi_danhSachDiemThi(PtrDT dsdt, string maLH, string mssv)
+{
+    string fileLocation = "Data/DanhSachSinhVien/DanhSachDiemThi/" + maLH + '/' + mssv + ".txt";
+    ofstream file(fileLocation);
+    if (!file)
+        return 0;
+    PtrDT p = dsdt;
+    while (p != NULL)
+    {
+        file << p->info.maMonHoc << '|' << p->info.diemThi << "|\n";
+        p = p->next;
+    }
+    return 1;
+}
 int ghi_danhSachMonHoc(ListMH dsmh)
 {
-    //Mo file
+    // Mo file
     ofstream file;
     file.open("Data/DanhSachLopHoc.txt", ios::out);
     if (!file)
         return 0;
 
     file << dsmh.n << endl;
-    
-    for(int i = 0; i < dsmh.n; i++)
+
+    for (int i = 0; i < dsmh.n; i++)
     {
         // cap du lieu
         MonHoc data;
-        file << data.maMonHoc << "|" ;
-		file << data.tenMonHoc << "|" << endl ;
+        file << data.maMonHoc << "|";
+        file << data.tenMonHoc << "|" << endl;
     }
     // Đong file
     file.close();
     return 1;
 }
-int ghi_danhSachCauHoi(STreeCH dsch){
+int ghi_danhSachCauHoi(STreeCH dsch)
+{
     ofstream ghifile("Data/DanhSachCauHoi.txt");
 
-    Queue<STreeCH>q;
+    Queue<STreeCH> q;
     STreeCH temp;
 
-    if(!ghifile)return 0;
+    if (!ghifile)
+        return 0;
     q.Push(dsch);
-    while(!q.empty())
+    while (!q.empty())
     {
-        temp=q.pop();
-        if(temp->left!=NULL)q.Push(temp->left);
-        if(temp->right!=NULL)q.Push(temp->right);
-        ghifile<<temp->info.maMonHoc<<endl<<temp->info.ID<<endl<<temp->info.question<<endl<<temp->info.ans1<<endl
-        <<temp->info.ans2<<endl<<temp->info.ans3<<endl<<temp->info.ans4<<temp->info.answer;
-        if(!q.empty())ghifile<<endl;
+        temp = q.pop();
+        if (temp->left != NULL)
+            q.Push(temp->left);
+        if (temp->right != NULL)
+            q.Push(temp->right);
+        ghifile << temp->info.maMonHoc << endl
+                << temp->info.ID << endl
+                << temp->info.question << endl
+                << temp->info.ans1 << endl
+                << temp->info.ans2 << endl
+                << temp->info.ans3 << endl
+                << temp->info.ans4 << temp->info.answer;
+        if (!q.empty())
+            ghifile << endl;
     }
     ghifile.close();
 }
--- ThiTracNghiem/docGhiFile.h
+++ ThiTracNghiem/docGhiFile.h
@@ -10,7 +10,7 @@
 #include "struct.h"
 
 using namespace std;
-/* 
+/*
 return:
 0:  khong mo duoc file dslh
 1:  ok
@@ -18,22 +18,21 @@ return:
 -2: khong doc duoc file cho dslk SinhVien
 -3: doc file cho dslk SinhVien bi loi~
 -4: khong doc duoc file cho dslk DiemThi
--5: doc file cho dslk DiemThi bi loi~   
+-5: doc file cho dslk DiemThi bi loi~
  */
 int doc_danhSachLopHoc(ListLH &dslh);
-int doc_danhSachSinhVien(PtrSV &dssv, string maLH); //--> doc_danhSachLopHoc
+int doc_danhSachSinhVien(PtrSV &dssv, string maLH);             //--> doc_danhSachLopHoc
 int doc_danhSachDiemThi(PtrDT &dsdt, string maLH, string mssv); // --> doc_danhSachSinhVien
 
 int doc_danhSachMonHoc(ListMH &dsmh);
 int doc_danhSachCauHoi(STreeCH &dsch);
 int doc_CauHoiDaThi(string maLH, string mssv);
-int kiemTraDangNhap(string user, string password);
 
- // <-- dssv, dsdt, dstk
+// <-- dssv, dsdt, dstk
 int ghi_danhSachLopHoc(ListLH dslh);
 int ghi_danhSachSinhVien(PtrSV dssv);
 int ghi_danhSachDiemThi(PtrDT dsdt);
-int ghi_danhSachTaiKhoan(ListLH dslh);
+int ghi_danhSachTaiKhoan(ListLH dslh); // ios::app
 int ghi_danhSachMonHoc(ListMH dsmh);
 int ghi_danhSachCauHoi(STreeCH dsch);
 
