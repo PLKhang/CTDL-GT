d41a213bf021bf43b983a20e0e148e1863b7cf8e
Lê Phúc Khang <plkhang090403@gmail.com>
2023-03-25 15:01:10 +0700

    update dohoa

---
+++ ThiTracNghiem/doHoa.cpp
@@ -0,0 +1,20 @@
+#include "doHoa.h"
+
+int ThongBao();
+bool XacNhan();
+void VeKhung();
+void VeOptions();
+
+void MENU_GV();
+void MENU_SV();
+void VeDanhSachLopHoc(ListLH dslh);
+void VeDanhSachSinhVien(PtrSV dssv);
+void VeDanhSachDiemThi(PtrDT dsdt);
+void veDanhSachMonHoc(ListMH dsmh);
+void VeDanhSachCauHoi();
+
+void KhungThemSinhVien();
+void KhungThemMonHoc();
+void KhungThemDiemThi();
+void KhungThemLopHoc();
+void KhungCauHoi();
