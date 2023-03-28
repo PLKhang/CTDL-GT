67311a76df07aadb099ddb4a94db4d88d2ce7fd7
Lê Phúc Khang <plkhang090403@gmail.com>
2023-03-28 22:40:19 +0700

    Yeu Phuong

--- ThiTracNghiem/main.cpp
+++ ThiTracNghiem/main.cpp
@@ -9,7 +9,7 @@ int main()
 	VeHeader(2, "DANH SACH SINH VIEN", "D21CQCN01-N CONG NGHE 1 KHOA 2021");
 	VeDanhSach(2);
 	gotoxy(6, 9); cout << "  MA SO SINH VIEN  ";
-	gotoxy(26, 9); cout << "                     HO                       ";
+	gotoxy(26, 9); cout << "                       HO                        ";
 	gotoxy(81, 9); cout << "     TEN      ";
 	gotoxy(101, 9); cout << "  GIOI TINH  ";
 	gotoxy(0,40); 
